0eda5f4b13c31f16b833abd2c732198cbbe1985d
Greaterthemassgreatertheattraction <JW268746@gmail.com>
2025-05-08 14:12:50 -0400

    Update Enemy.cs
    Fixed error

--- Enemy.cs
+++ Enemy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy_AI_Movement : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     public Transform player;
     public float AttackDistance;
@@ -32,76 +32,39 @@ public class Enemy_AI_Movement : MonoBehaviour
     void Update()
     {
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-        playerInAttackRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
         if (!playerInSightRange && !playerInAttackRange) Patroling();
         if (playerInSightRange && !playerInAttackRange) Chasing();
         if (playerInSightRange && playerInAttackRange) Attacking();
-        //attack animation + follow player
-        /*m_Distance = Vector3.Distance(agent.transform.position, player.position);
-        
-        if (m_Distance < AttackDistance)
+    }
+    void Patroling()
+    {
+        if(!walkPointSet)
         {
-            agent.isStopped = true;
-            //m_Animator.SetBool("Attack", true);
+            float randomZ = Random.Range(-walkPointRange, walkPointRange);
+            float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+            WalkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+            if (Physics.Raycast(WalkPoint, -transform.up, 2f, whatIsGround)) walkPointSet = true;
         }
-        else
-        {
-            agent.isStopped = false;
-            //m_Animator.SetBool("Attack", false); 
-            agent.destination = player.position;
-        }
+        if (walkPointSet) agent.SetDestination(WalkPoint);
 
-        //movement animation
-        if (agent.velocity.magnitude != 0f)
-        {
-            //m_Animator.SetBool("Running", true);
-        }
-        else
-        {
-            //m_Animator.SetBool("Ruinning", false);
-        }
-        */
-        /*void OnAnimatorMove()
-        {
-            if (m_Animator.GetBool("Running"))
-            {
-                agent.speed = (m_Animator.deltaPosition / Time.deltaTime).magnitude;
-            }
-            if (m_Animator.GetBool("Attack") == false)
-            {
-                agent.speed = (m_Animator.deltaPosition / Time.deltaTime).magnitude;
-            }
-        } */
-        void Patroling()
-        {
-            if(!walkPointSet)
-            {
-                float randomZ = Random.Range(-walkPointRange, walkPointRange);
-                float randomX = Random.Range(-walkPointRange, walkPointRange);
+        Vector3 distanecToWalkPoint = transform.position - WalkPoint;
 
-                WalkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
-                if (Physics.Raycast(WalkPoint, -transform.up, 2f, whatIsGround)) walkPointSet = true;
-            }
-            if (walkPointSet) agent.SetDestination(WalkPoint);
-
-            Vector3 distanecToWalkPoint = transform.position - WalkPoint;
-
-            if (distanecToWalkPoint.magnitude < 1f) walkPointSet = false;
+        if (distanecToWalkPoint.magnitude < 1f) walkPointSet = false;
 
 
-        }
-        void Chasing()
-        {
-            agent.SetDestination(player.position);
-        }
-        void Attacking()
-        {
-            agent.SetDestination(transform.position);
+    }
+    void Chasing()
+    {
+        agent.SetDestination(player.position);
+    }
+    void Attacking()
+    {
+        agent.SetDestination(transform.position);
 
-            transform.LookAt(player);
-        }
-
+        transform.LookAt(player);
     }
 }
